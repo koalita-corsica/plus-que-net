c6f54de53e87272eba69435ec3018c6f8f2b3f88
Boccanfuso Raphael <raphael.boccanfuso@gmail.com>
2021-04-13 15:50:46 +0200

    header

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -7,34 +7,30 @@ import styles from './header.module.css'
 
 const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
   <div className={styles.root}>
-    <div className={styles.wrapper}>
-      <div className={styles.branding}>
-        <Link to='/'>{siteTitle}</Link>
-      </div>
-
-      <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
-        <Icon symbol='hamburger' />
-      </button>
-
-      <nav className={cn(styles.nav, showNav && styles.showNav)}>
-        <ul>
-          <li>
-            <Link to='/'>Accueil</Link>
-          </li>
-          <li>
-            <Link to='/tarifs/'>Prestations e Tarifs</Link>
-          </li>
-          <li>
-            <Link to='/blog/'>Blog</Link>
-          </li>
-          <li>
-            <Link to='/partenaires/'>Partenaires</Link>
-          </li>
-          <li>
-            <Link to='/contact/'>Contact</Link>
-          </li>
-        </ul>
-      </nav>
+    <div className={styles.logo}>
+      <img src="https://drive.google.com/file/d/1htYVOZQwr7arnRZveyT4zZhj2YhFwysE/view?ts=60759839" alt="Plus-que-net" id="logo"/>
+    </div>
+    <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
+      <Icon symbol='hamburger' />
+    </button>
+    <div className={styles.nav}>
+      <ul>
+        <li>
+          <Link to='/'>Accueil</Link>
+        </li>
+        <li>
+          <Link to='/tarifs/'>Prestations e Tarifs</Link>
+        </li>
+        <li>
+          <Link to='/blog/'>Blog</Link>
+        </li>
+        <li>
+          <Link to='/partenaires/'>Partenaires</Link>
+        </li>
+        <li>
+          <Link to='/contact/'>Contact</Link>
+        </li>
+      </ul>
     </div>
   </div>
 )
