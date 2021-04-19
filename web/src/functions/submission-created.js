d7bc3e5b6f1cea433b46e9e11135567c75777a80
Tiago Carvalho <devtiagocarvalho24@gmail.com>
2021-04-19 18:51:04 +0200

    form

--- web/src/functions/submission-created.js
+++ web/src/functions/submission-created.js
@@ -1,10 +1,11 @@
 const sanityClient = require("@sanity/client")
 const client = sanityClient({
-  projectId: 'xlw4ib3d',
-  dataset: 'production',
-  token: 'skNVUTpttzNTGl5X66WrNGkzaXpxOB01XUr7JuAvUgk0xiiiFgoAtPZeKdlmk0LEDsB2Ykqo2qYut35ryzbt2g7RJPQ1E9sAlhO25Dyi4NIkg1pBB9YSWosc8UXF25PweVrX24ivGTh4q2T07qSKC7G2AKB61Niu5ORz2PLV1PSqzarfChzr',
+  projectId: process.env.GATSBY_SANITY_PROJECT_ID,
+  dataset: process.env.GATSBY_SANITY_PROJECT_DATASET,
+  token: process.env.GATSBY_SANITY_TOKEN,
   useCDN: false,
 })
+
 exports.handler = async function (event, context, callback) {
   // Pulling out the payload from the body
   const { payload } = JSON.parse(event.body)
