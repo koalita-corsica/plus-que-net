f4f3ce8f4339ff99201079076896fa33346cde36
Tiago Carvalho <devtiagocarvalho24@gmail.com>
2021-04-19 15:24:47 +0200

    dd

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,8 +1,8 @@
 const sanityClient = require('@sanity/client')
 const client = sanityClient({
-  projectId: 'zv292tg5',
+  projectId: 'xlw4ib3d',
   dataset: 'production',
-  token: process.env.SANITY_TOKEN
+  token: 'skNVUTpttzNTGl5X66WrNGkzaXpxOB01XUr7JuAvUgk0xiiiFgoAtPZeKdlmk0LEDsB2Ykqo2qYut35ryzbt2g7RJPQ1E9sAlhO25Dyi4NIkg1pBB9YSWosc8UXF25PweVrX24ivGTh4q2T07qSKC7G2AKB61Niu5ORz2PLV1PSqzarfChzr'
 })
 
 exports.handler = async function (event, context, callback) {
