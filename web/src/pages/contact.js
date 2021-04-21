e6480f56cc10ca8617dc5b5e370f78d89903ac23
Boccanfuso Raphael <raphael.boccanfuso@gmail.com>
2021-04-21 11:58:13 +0200

    lol

--- web/src/pages/contact.js
+++ web/src/pages/contact.js
@@ -1,144 +1,144 @@
-import React from 'react'
-import {graphql} from 'gatsby'
-import {
-  mapEdgesToNodes,
-  filterOutDocsWithoutSlugs,
-  filterOutDocsPublishedInTheFuture
-} from '../lib/helpers'
-import BlogPostPreviewList from '../components/blog-post-preview-list'
-import Container from '../components/container'
-import GraphQLErrorList from '../components/graphql-error-list'
-import SEO from '../components/seo'
-import Layout from '../containers/layout'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook } from "@fortawesome/free-brands-svg-icons"
-import { faInstagram } from "@fortawesome/free-brands-svg-icons"
-import { faFacebookMessenger } from "@fortawesome/free-brands-svg-icons"
-import { faWhatsapp } from "@fortawesome/free-brands-svg-icons"
-import { useForm } from "react-hook-form"
+// import React from 'react'
+// import {graphql} from 'gatsby'
+// import {
+//   mapEdgesToNodes,
+//   filterOutDocsWithoutSlugs,
+//   filterOutDocsPublishedInTheFuture
+// } from '../lib/helpers'
+// import BlogPostPreviewList from '../components/blog-post-preview-list'
+// import Container from '../components/container'
+// import GraphQLErrorList from '../components/graphql-error-list'
+// import SEO from '../components/seo'
+// import Layout from '../containers/layout'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faFacebook } from "@fortawesome/free-brands-svg-icons"
+// import { faInstagram } from "@fortawesome/free-brands-svg-icons"
+// import { faFacebookMessenger } from "@fortawesome/free-brands-svg-icons"
+// import { faWhatsapp } from "@fortawesome/free-brands-svg-icons"
+// import { useForm } from "react-hook-form"
 
-import styles from './contact.module.css'
+// import styles from './contact.module.css'
 
-import {isBrowser} from '../lib/utils'
+// import {isBrowser} from '../lib/utils'
 
-export const query = graphql`
+// export const query = graphql`
 
-  query ContactPageQuery {
-  page: sanityPage(slug: {current: {eq: "contact"}}) {
-    id
-    title
-    authors {
-      author {
-        name
-      }
-    }
-  }
-}
-`
+//   query ContactPageQuery {
+//   page: sanityPage(slug: {current: {eq: "contact"}}) {
+//     id
+//     title
+//     authors {
+//       author {
+//         name
+//       }
+//     }
+//   }
+// }
+// `
 
-const ContactPage = props => {
-  if (!isBrowser) {
-    return;
-  }
+// const ContactPage = props => {
+//   if (!isBrowser) {
+//     return;
+//   }
 
-  const { register, handleSubmit, errors, reset } = useForm()
+//   const { register, handleSubmit, errors, reset } = useForm()
   
 
-  // Transforms the form data from the React Hook Form output to a format Netlify can read
-  const encode = (data) => {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&")
-  }
+//   // Transforms the form data from the React Hook Form output to a format Netlify can read
+//   const encode = (data) => {
+//     return Object.keys(data)
+//       .map(
+//         (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+//       )
+//       .join("&")
+//   }
 
-  // Handles the post process to Netlify so we can access their serverless functions
-  const handlePost = (formData, event) => {
-    fetch(`/`, {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact-form", ...formData }),
-    })
-      .then((response) => {
-        navigate("/success/")
-        reset()
-        console.log(response)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-    event.preventDefault()
-  }
+//   // Handles the post process to Netlify so we can access their serverless functions
+//   const handlePost = (formData, event) => {
+//     fetch(`/`, {
+//       method: "POST",
+//       headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//       body: encode({ "form-name": "contact-form", ...formData }),
+//     })
+//       .then((response) => {
+//         navigate("/success/")
+//         reset()
+//         console.log(response)
+//       })
+//       .catch((error) => {
+//         console.log(error)
+//       })
+//     event.preventDefault()
+//   }
 
 
-  const {data} = props
-  const page = data && data.page;
-  if (errors) {
-    return (
-      <Layout>
-        <GraphQLErrorList errors={errors} />
-      </Layout>
-    )
-  }
+//   const {data} = props
+//   const page = data && data.page;
+//   if (errors) {
+//     return (
+//       <Layout>
+//         <GraphQLErrorList errors={errors} />
+//       </Layout>
+//     )
+//   }
 
-  return (
-    <form
-      onSubmit={handleSubmit(handlePost)}
-      name="contact-form"
-      method="POST"
-      action="/success/"
-      data-netlify="true"
-      netlify-honeypot="got-ya"
-    >
-      <input type="hidden" name="form-name" value="contact-form" />
-      <input
-        type="hidden"
-        name="formId"
-        value="contact-form"
-        ref={register()}
-      />
-      <label htmlFor="name">
-        <p>Name</p>
-        {errors.name && <span>Error message</span>}
-        <input name="name" ref={register({ required: true })} />
-      </label>
-      <label htmlFor="email">
-        <p>Email</p>
-        {errors.email && <span>Please format email correctly</span>}
-        <input
-          name="email"
-          ref={register({
-            required: true,
-            pattern: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
-          })}
-        />
-      </label>
-      <label htmlFor="message">
-        <p>Message</p>
-        <textarea rows="4" name="message" ref={register()} />
-      </label>
-      <label
-        htmlFor="got-ya"
-        style={{
-          position: "absolute",
-          overflow: "hidden",
-          clip: "rect(0 0 0 0)",
-          height: "1px",
-          width: "1px",
-          margin: "-1px",
-          padding: "0",
-          border: "0",
-        }}
-      >
-        Don’t fill this out if you're human:
-        <input tabIndex="-1" name="got-ya" ref={register()} />
-      </label>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
-  )
-}
+//   return (
+//     <form
+//       onSubmit={handleSubmit(handlePost)}
+//       name="contact-form"
+//       method="POST"
+//       action="/success/"
+//       data-netlify="true"
+//       netlify-honeypot="got-ya"
+//     >
+//       <input type="hidden" name="form-name" value="contact-form" />
+//       <input
+//         type="hidden"
+//         name="formId"
+//         value="contact-form"
+//         ref={register()}
+//       />
+//       <label htmlFor="name">
+//         <p>Name</p>
+//         {errors.name && <span>Error message</span>}
+//         <input name="name" ref={register({ required: true })} />
+//       </label>
+//       <label htmlFor="email">
+//         <p>Email</p>
+//         {errors.email && <span>Please format email correctly</span>}
+//         <input
+//           name="email"
+//           ref={register({
+//             required: true,
+//             pattern: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+//           })}
+//         />
+//       </label>
+//       <label htmlFor="message">
+//         <p>Message</p>
+//         <textarea rows="4" name="message" ref={register()} />
+//       </label>
+//       <label
+//         htmlFor="got-ya"
+//         style={{
+//           position: "absolute",
+//           overflow: "hidden",
+//           clip: "rect(0 0 0 0)",
+//           height: "1px",
+//           width: "1px",
+//           margin: "-1px",
+//           padding: "0",
+//           border: "0",
+//         }}
+//       >
+//         Don’t fill this out if you're human:
+//         <input tabIndex="-1" name="got-ya" ref={register()} />
+//       </label>
+//       <div>
+//         <button type="submit">Submit</button>
+//       </div>
+//     </form>
+//   )
+// }
 
-export default ContactPage
+// export default ContactPage
--- web/src/pages/prestations-tarifs.js
+++ web/src/pages/prestations-tarifs.js
@@ -18,7 +18,7 @@ import { faCity } from '@fortawesome/free-solid-svg-icons'
 import { faBrush } from '@fortawesome/free-solid-svg-icons'
 import { faShower } from '@fortawesome/free-solid-svg-icons'
 import { faDungeon } from '@fortawesome/free-solid-svg-icons'
-
+import Gallery from '../components/gallery'
 import {isBrowser} from '../lib/utils'
 
 
@@ -66,6 +66,87 @@ const PrestationPage = props => {
     return;
   }
 
+  
+
+
+
+
+  //movenment controls
+  function rotateClick4() {
+    //Variable
+    const icons1 = document.querySelector('.iconContent1');
+    console.log(icons1);
+    const icons2 = document.querySelector('.iconContent2');
+    console.log(icons2);
+    const icons3 = document.querySelector('.iconContent3');
+    console.log(icons3);
+    const icons4 = document.querySelector('.iconContent4');
+    console.log(icons4);
+    icons4.setAttribute("style", 'grid-area: 1 / 2 / 2 / 3;');
+    icons3.setAttribute("style", 'grid-area: 2 / 2 / 3 / 3;');
+    icons1.setAttribute("style", 'grid-area: 2 / 1 / 3 / 2;');
+    icons2.setAttribute("style", 'grid-area: 1 / 1 / 2 / 2;');
+    return "ok";
+  };
+  function rotateClick3() {
+    //Variable
+    const icons1 = document.querySelector('.iconContent1');
+    console.log(icons1);
+    const icons2 = document.querySelector('.iconContent2');
+    console.log(icons2);
+    const icons3 = document.querySelector('.iconContent3');
+    console.log(icons3);
+    const icons4 = document.querySelector('.iconContent4');
+    console.log(icons4);
+    icons3.setAttribute("style", 'grid-area: 1 / 2 / 2 / 3;' );
+    icons2.setAttribute("style", 'grid-area: 2 / 1 / 3 / 2;' );
+    icons1.setAttribute("style", 'grid-area: 2 / 2 / 3 / 3;' );
+    icons4.setAttribute("style", 'grid-area: 1 / 1 / 2 / 2;' );
+    return "ok";
+  };
+  function rotateClick2() {
+    //Variable
+    const icons1 = document.querySelector('.iconContent1');
+    console.log(icons1);
+    const icons2 = document.querySelector('.iconContent2');
+    console.log(icons2);
+    const icons3 = document.querySelector('.iconContent3');
+    console.log(icons3);
+    const icons4 = document.querySelector('.iconContent4');
+    console.log(icons4);
+    icons2.setAttribute("style", 'grid-area: 1 / 2 / 2 / 3;' );
+    icons1.setAttribute("style", 'grid-area: 1 / 1 / 2 / 2;' );
+    icons3.setAttribute("style", 'grid-area: 2 / 1 / 3 / 2;' );
+    icons4.setAttribute("style", 'grid-area: 2 / 2 / 3 / 3;' );
+    return "ok";
+  };
+  function rotateClick1() {
+    //Variable
+    const icons1 = document.querySelector('.iconContent1');
+    console.log(icons1);
+    const icons2 = document.querySelector('.iconContent2');
+    console.log(icons2);
+    const icons3 = document.querySelector('.iconContent3');
+    console.log(icons3);
+    const icons4 = document.querySelector('.iconContent4');
+    console.log(icons4);
+    icons1.setAttribute("style", 'grid-area: 1 / 2 / 2 / 3;' );
+    icons2.setAttribute("style", 'grid-area: 2 / 2 / 3 / 3;' );
+    icons4.setAttribute("style", 'grid-area: 2 / 1 / 3 / 2;' );
+    icons3.setAttribute("style", 'grid-area: 1 / 1 / 2 / 2;' );
+    return "ok";
+  };
+  
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('Le lien a été cliqué.');
+    <Gallery></Gallery>
+  }
+  
+
+
+
+
   const {data, errors} = props
   const page = data && data.services;
   const service = data && data.service;
@@ -77,8 +158,9 @@ const PrestationPage = props => {
       </Layout>
     )
   }
-
+  
   return (
+    
     <Layout>
       <Container>
         <div className={styles.titleContain}>
@@ -88,22 +170,31 @@ const PrestationPage = props => {
           <div className={styles.BlockContent}>
             <div className={styles.menuContent}>
               <div className={styles.iconeMenu}>
-                <div className={styles.icone}><FontAwesomeIcon icon={faCity} className={styles.icon}/></div>
-                <div className={styles.icone}><FontAwesomeIcon icon={faBrush} className={styles.icon}/></div>
-                <div className={styles.icone}><FontAwesomeIcon icon={faShower} className={styles.icon}/></div>
-                <div className={styles.icone}><FontAwesomeIcon icon={faDungeon} className={styles.icon}/></div>
+                <div className={styles.iconContent1} onClick={rotateClick1} class="iconContent1">
+                  <span className={styles.icone1}><FontAwesomeIcon icon={faCity} className={styles.icon}/></span>
+                  </div>
+                <div className={styles.iconContent2} onClick={rotateClick2} class="iconContent2">
+                  <span className={styles.icone2}><FontAwesomeIcon icon={faBrush} className={styles.icon}/></span>
+                </div>
+                <div className={styles.iconContent3} onClick={rotateClick3} class="iconContent3">
+                  <span className={styles.icone3}><FontAwesomeIcon icon={faShower} className={styles.icon}/></span>
+                </div>
+                <div className={styles.iconContent4} onClick={rotateClick4} class="iconContent4">
+                  <span className={styles.icone4}><FontAwesomeIcon icon={faDungeon} className={styles.icon}/></span>
+                </div>
               </div>
               <div className={styles.titleMenu}>
                 <h2 className={styles.title}>Lavage de vitres simples</h2>
               </div>
             </div>
             <div className={styles.description}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
-            <button className={styles.button}>Voir la galerie</button>
+            <button className={styles.button} onClick={handleClick}>Voir la galerie</button>
           </div>
         </Wrapper>
       </Container>
     </Layout>
   )
+  
 }
 
 export default PrestationPage
