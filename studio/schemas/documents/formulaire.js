04853fc74be91738a1ccb4c5fee1b7ab9eeccdf5
Tiago Carvalho <devtiagocarvalho24@gmail.com>
2021-04-22 09:59:45 +0200

    form

--- studio/schemas/documents/formulaire.js
+++ studio/schemas/documents/formulaire.js
@@ -1,32 +1,51 @@
 export default {
-    name: "contact",
-    title: "Contact submissions",
-    type: "document",
-    fields: [
-      {
-        name: "name",
-        title: "Name",
-        type: "string",
-      },
-      {
-        name: "email",
-        title: "Email",
-        type: "string",
-      },
-      {
-        name: "message",
-        title: "Message",
-        type: "text",
-      },
-      {
-        name: "adresse",
-        title: "Adresse",
-        type: "string",
-      },
-      {
-        name: "telephone",
-        title: "Telephone",
-        type: "number",
-      }
-    ],
-  }
+  name: 'submission.form',
+  type: 'document',
+  title: 'Form submission',
+  readOnly: true,
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title'
+    },
+    {
+      name: 'number',
+      type: 'number',
+      title: 'Number'
+    },
+    {
+      name: 'created_at',
+      type: 'datetime',
+      title: 'Created at'
+    },
+    {
+      name: 'data',
+      type: 'object',
+      title: 'Data',
+      fields: [
+        {
+          name: 'email',
+          type: 'email',
+          title: 'Email'
+        },
+        {
+          name: 'name',
+          type: 'string',
+          title: 'Name'
+        },
+        {
+          name: 'message',
+          type: 'text',
+          title: 'Message'
+        },
+        {
+          name: 'role',
+          type: 'array',
+          title: 'Role',
+          of: [{ type: 'string' }]
+        }
+      ]
+    }
+  ]
+}
--- web/src/components/gallery.js
+++ web/src/components/gallery.js
@@ -4,8 +4,8 @@ import GraphQLErrorList from '../components/graphql-error-list'
 import { StaticQuery, graphql } from "gatsby"
 
 
-// import "slick-carousel/slick/slick.css"; 
-// import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 import styles from './galerie.module.css'
 import Wrapper from "./wrapper";
 
@@ -17,7 +17,9 @@ const settings = {
 };
 
 
+
 const SimpleSlider = ({ data, settings }) => (
+
       <Wrapper>
             <React.Fragment>
               {console.log(data.sanityServices.images)}
