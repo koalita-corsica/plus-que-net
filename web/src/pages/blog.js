aee1037301eece06e41a28fdac8fa3edc56dc93d
Tiago Carvalho <devtiagocarvalho24@gmail.com>
2021-04-22 16:15:43 +0200

    go

--- web/src/pages/blog.js
+++ web/src/pages/blog.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import {graphql} from 'gatsby'
 import {
   mapEdgesToNodes,
   filterOutDocsWithoutSlugs,
@@ -15,10 +15,8 @@ import WrapperBlog from '../components/wrapperBlog'
 import styles from '../pages/blog.module.css'
 import imgTest from '../images/test.jpg'
 import Block from '@sanity/block-content-to-react'
-import { buildImageObj, cn, getBlogUrl } from '../lib/helpers'
-import { Link } from 'gatsby'
 
-import { isBrowser } from '../lib/utils'
+import {isBrowser} from '../lib/utils'
 
 export const query = graphql`
   query BlogPageQuery {
@@ -35,7 +33,6 @@ export const query = graphql`
     ) {
     edges {
       node {
-        _id
         id
         publishedAt
         mainImage {
@@ -61,10 +58,9 @@ const BlogPage = props => {
     return;
   }
 
-  const { data, errors } = props
+  const {data, errors} = props
   const page = data && data.page;
   const blogPost = data && data.posts;
-  const articles = []
   if (errors) {
     return (
       <Layout>
@@ -72,8 +68,8 @@ const BlogPage = props => {
       </Layout>
     )
   }
-
-
+  
+  
   const postNodes = data && data.posts && mapEdgesToNodes(data.posts)
 
   return (
@@ -83,46 +79,32 @@ const BlogPage = props => {
           <h1 className={styles.title}>{page.title}</h1>
         </div>
         <WrapperBlog>
-          {postNodes.forEach(element => {
-            articles.push(element)
-          })}
-          <ul>
-            <div className={styles.blogContain}>
-              <h2 className={styles.allArticle}>Tous les articles</h2>
-              {articles.forEach(element => {
-                  <React.Fragment>
-
-                    <div key={element.id} className={styles.lastArticle}>
-                      <li key={element.mainImage.asset._id}>
-                        <img src={element.mainImage.asset.url} className={styles.imgLastArticle}></img>
-                      </li>
-                      <div key={element.id} className={styles.lastArticleContain}>
-                        <span className={styles.date}> {element.publishedAt} </span>
-                        <h3 className={styles.articleTitle}> {element.title} </h3>
-                        <span className={styles.description}> {element._rawExcerpt} </span>
-                      </div>
-                    </div>
-
-                    <div key={element.id} className={styles.Article}>
-                      <li key={element.mainImage.asset._id}>
-                        <img src={element.mainImage.asset.url} className={styles.imgLastArticle}></img>
-                      </li>
-                      <div key={element.id} className={styles.lastArticleContain}>
-                        <span className={styles.date}> {element.publishedAt} </span>
-                        <h3 className={styles.articleTitle}> {element.title} </h3>
-                        <span className={styles.description}> {element._rawExcerpt} </span>
-                      </div>
-                    </div>
-                  </React.Fragment>
-              })}
-            </div>
-          </ul>
+          {blogPost.edges.map(( dados =>
+            <React.Fragment>
+              {console.log(dados.node.mainImage.asset.url)}
+          <div className={styles.blogContain}>
+            <h2 className={styles.allArticle}>Tous les articles</h2>
+            <div className={styles.lastArticle}>
+              <img  src={dados.node.mainImage.asset.url} alt={dados.node.mainImage.alt} width="389" height="258" className={styles.imgLastArticle}></img>
+              <div className={styles.lastArticleContain}>
+                <p className={styles.date}> {dados.node.publishedAt} </p>
+                <h3 className={styles.articleTitle}> {dados.node.title} </h3>
+                <p className={styles.description}> <Block blocks={dados.node._rawExcerpt} /> </p>
+              </div>
+            </div>      
+            <div className={styles.Article}>
+              <img src={dados.node.mainImage.asset.url} alt={dados.node.mainImage.alt} width="289" height="169"></img>
+              <p className={styles.date}> {dados.node.publishedAt} </p>
+              <h3 className={styles.articleTitle}> {dados.node.title} </h3>
+              <p className={styles.description}> <Block blocks={dados.node._rawExcerpt} /> </p>
+            </div>      
+          </div>      
           <aside className={styles.aside}>
-            <h3 className={styles.categorieTitle} >Thématique</h3>
-            <div className={styles.categorie}>
-              <h2>Thèmes</h2>
-            </div>
+            <h3 className={styles.categorie} >Thématique</h3>
+            <img src="" width="246" height="163"></img>
           </aside>
+          </React.Fragment>
+          ))}
         </WrapperBlog>
       </Container>
     </Layout>
@@ -130,37 +112,3 @@ const BlogPage = props => {
 }
 
 export default BlogPage
-
-
-{/* <Layout>
-<Container>
-  <div className={styles.titleContain}>
-    <h1 className={styles.title}>{page.title}</h1>
-  </div>
-  <WrapperBlog>
-    {postNodes.forEach(element => {
-      articles.push(element)
-    })}
-       <React.Fragment>
-     <div className={styles.blogContain}>
-       <h2 className={styles.allArticle}>Tous les articles</h2>
-       <Link to={getBlogUrl(articles[0].publishedAt, articles[0].slug.current)} >
-       <div className={styles.lastArticle}>
-        <img  src={articles[0].mainImage.asset.url} alt={articles[0].mainImage.alt} width="389" height="258" className={styles.imgLastArticle}></img>
-         <div className={styles.lastArticleContain}>
-           <p className={styles.date}> {articles[0].publishedAt} </p>
-           <h3 className={styles.articleTitle}> {articles[0].title} </h3>
-           <p className={styles.description}> <Block blocks={articles[0]._rawExcerpt} /> </p>
-         </div>
-       </Link>
-       {articles.forEach(element => {
-        <Link to={getBlogUrl(element.publishedAt, element.slug.current)} >
-          <div className={styles.Article}>
-            <img src={element.mainImage.asset.url} alt={element.mainImage.alt} width="289" height="169"></img>
-            <p className={styles.date}> {element.publishedAt} </p>
-            <h3 className={styles.articleTitle}> {element.title} </h3>
-            <p className={styles.description}> <Block blocks={element._rawExcerpt} /> </p>
-          </div>
-        </Link>
-       })}
-      </div>    */}
