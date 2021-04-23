ac0ebaa833149e8f81cda3a72aaffe7b55efcd72
Boccanfuso Raphael <raphael.boccanfuso@gmail.com>
2021-04-23 15:23:39 +0200

    contact

--- web/src/pages/blog.js
+++ web/src/pages/blog.js
@@ -79,17 +79,16 @@ const BlogPage = props => {
           <h1 className={styles.title}>{page.title}</h1>
         </div>
         <WrapperBlog>
-            <React.Fragment>
-              <div className={styles.blogContain}>
+          <div className={styles.blogContain}>
             <h2 className={styles.allArticle}>Tous les articles</h2>
-              <div className={styles.lastArticle}>
-                  <img  src={imgTest}  className={styles.imgLastArticle}></img>
-                    <div className={styles.lastArticleContain}>
-                        <span className={styles.date}>23 juin 2020</span>
-                          <h3 className={styles.articleTitle}>Last article title</h3>
-                        <span className={styles.description}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span>
-                    </div>
-              </div>      
+            <div className={styles.lastArticle}>
+              <img  src={imgTest}  className={styles.imgLastArticle}></img>
+                <div className={styles.lastArticleContain}>
+                  <span className={styles.date}>23 juin 2020</span>
+                    <h3 className={styles.articleTitle}>Last article title</h3>
+                  <span className={styles.description}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span>
+                </div>
+            </div>      
             <div className={styles.Article}>
               <img src={imgTest} className={styles.imgArticle}></img>
               <div className={styles.articleContain}>
@@ -140,10 +139,19 @@ const BlogPage = props => {
             </div>      
           </div>      
           <aside className={styles.aside}>
-            <h3 className={styles.categorie} >Thématique</h3>
-            <img src="" width="246" height="163"></img>
+            <h2 className={styles.categorieTitle} >Thématique</h2>
+            <div className={styles.catWrapper}>
+              <div className={styles.categorie}>
+                <h2>Thèmes</h2>
+              </div>
+              <div className={styles.categorie}>
+                <h2>Thèmes</h2>
+              </div>
+              <div className={styles.categorie}>
+                <h2>Thèmes</h2>
+              </div>
+            </div>
           </aside>
-          </React.Fragment>
         </WrapperBlog>
       </Container>
     </Layout>
--- web/src/pages/contact.js
+++ web/src/pages/contact.js
@@ -16,7 +16,7 @@ import { faInstagram } from "@fortawesome/free-brands-svg-icons"
 import { faFacebookMessenger } from "@fortawesome/free-brands-svg-icons"
 import { faWhatsapp } from "@fortawesome/free-brands-svg-icons"
 import { useForm } from "react-hook-form"
-import WrapperContact from '../components/wrapperContact'
+
 
 import styles from './contact.module.css'
 
@@ -53,95 +53,91 @@ const ContactPage = props => {
 //   }
 
   return (
+    // <Layout>
+    //      <form name="contact" method="POST" data-netlify="true">
+    //         <div class="field">
+    //           <label class="label">Your Name:
+    //             <input class="input" type="text" name="name" />
+    //           </label>
+    //         </div>
+    //         <div class="field">
+    //           <label class="label">Your Email:
+    //             <input class="input" type="email" name="email" />
+    //           </label>
+    //         </div>
+    //         <div class="field">
+    //           <label for="role[]" class="label">Your Role:</label>
+    //           <div class="select is-multiple">
+    //             <select name="role[]" multiple size="2">
+    //               <option value="leader">Leader</option>
+    //               <option value="follower">Follower</option>
+    //             </select>
+    //           </div>
+    //         </div>
+    //         <div class="field">
+    //           <label class="label">Message:
+    //             <textarea class="textarea" name="message"></textarea>
+    //           </label>
+    //         </div>
+    //         <div class="field">
+    //           <button class="button is-primary is-medium" type="submit">Send</button>
+    //         </div>
+    //       </form>
+    // </Layout>
     <Layout>
-         <form name="contact" method="POST" data-netlify="true">
-            <div class="field">
-              <label class="label">Your Name:
-                <input class="input" type="text" name="name" />
-              </label>
+        <Container>
+            <div className={styles.titleContain}>
+                <h1 className={styles.title}>Contact</h1>
             </div>
-            <div class="field">
-              <label class="label">Your Email:
-                <input class="input" type="email" name="email" />
-              </label>
-            </div>
-            <div class="field">
-              <label for="role[]" class="label">Your Role:</label>
-              <div class="select is-multiple">
-                <select name="role[]" multiple size="2">
-                  <option value="leader">Leader</option>
-                  <option value="follower">Follower</option>
-                </select>
-              </div>
-            </div>
-            <div class="field">
-              <label class="label">Message:
-                <textarea class="textarea" name="message"></textarea>
-              </label>
-            </div>
-            <div class="field">
-              <button class="button is-primary is-medium" type="submit">Send</button>
-            </div>
-          </form>
-    </Layout>
+            <div className={styles.contactWrapper}>
+              <form name="contact" method="POST" data-netlify="true" onSubmit="submit">
+                  <input type="hidden" name="form-name" value="contact" />
+                  <div className={styles.container}>
+                      <div className={styles.first}>
+                          <button className={styles.contact}> contact </button>
+                          <button className={styles.devis}> demande de devis </button>
+                          <div className={styles.social}>
+                            <FontAwesomeIcon icon={faFacebook} className={styles.fb}/>
+                            <FontAwesomeIcon icon={faInstagram} className={styles.insta}/>
+                            <FontAwesomeIcon icon={faFacebookMessenger} className={styles.messenger}/>
+                            <FontAwesomeIcon icon={faWhatsapp} className={styles.whatsapp}/>
+                          </div>
+                      </div>
+                      <div className={styles.second}>
+                          <label for="name"> nom prenom</label>
+                          <label for="numero"> téléphone</label>
+                          <label for="photo" className={styles.jointe}> pièces jointes </label>
+                          <input name="name" placeholder="Votre Nom Prenom" />
+                          <input name="numero" placeholder="Votre Numero" />  
+                          <input name="photo" placeholder="maphoto.jpg" /> 
+                      </div>
+                      <div className={styles.third}>
+                        <div className={styles.mail}>
+                            <label for="mail"> email </label>
+                            <input name="mail" placeholder="Votre mail" />
+                            <label for="adresse"> adresse </label>
+                            <input name="adresse" placeholder="Votre Adresse" />
+                        </div>
+                        <div className={styles.msg}>
+                          <label for="message"> message </label>
+                          <textarea name="message" placeholder="Votre Message" rows="6"/>
+                        </div>
+                        <div className={styles.preview}>
+                            <div className={styles.preview2}>
+                                <img src="https://imgur.com/gallery/EembI94" alt="s"className={styles.imgPreview}/>
+                                <button className={styles.addBtn}>+</button>
+                            </div>
+                            <button type="submit" className={styles.btnEnvoyer}> Envoyer! </button>  
+                        </div>
+                      </div>
+                  </div> 
+              </form>
+          </div>
+    </Container>
+  </Layout>
 )
   }
 
 export default ContactPage
 
 
-    // <Layout>
-    //   <Container>
-    //       <wrapperContact>
-    //         <div className={styles.contactWrapper}>
-    //             <form name="contact" method="POST" data-netlify="true" onSubmit="submit">
-    //                 <input type="hidden" name="form-name" value="contact" />
-    //                 <div className={styles.title}>
-    //                     <h1> {page.title} </h1>
-    //                 </div>
-    //                 <div className={styles.container}>
-    //                     <div className={styles.first}>
-    //                         <button> CONTACT </button>
-    //                         <button> DEMANDE DEVIS </button>
-    //                         <a><FontAwesomeIcon icon={faFacebook} className={styles.fb}/></a>
-    //                         <a><FontAwesomeIcon icon={faInstagram} className={styles.insta}/></a>
-    //                         <a><FontAwesomeIcon icon={faFacebookMessenger} className={styles.messenger}/></a>
-    //                         <a><FontAwesomeIcon icon={faWhatsapp} className={styles.whatsapp}/></a>
-    //                     </div>
-    //                     <div className={styles.second}>
-    //                         <label for="name"> NOM PRENOM</label>
-    //                         <label for="numero"> TELEPHONE</label>
-    //                         <label for="photo"> PIECES JOINTES </label>
-    //                     </div>
-    //                     <div className={styles.third}>
-    //                         <input name="name" placeholder="Votre Nom Prenom" />
-    //                         <input name="numero" placeholder="Votre Numero" />  
-    //                         <input name="photo" placeholder="maphoto.jpg" /> 
-    //                     </div>
-    //                     <div className={styles.fourth}>
-    //                         <label for="mail"> MAIL </label>
-    //                         <label for="message"> MESSAGE </label>
-    //                     </div>
-    //                     <div className={styles.five}>
-    //                         <textarea name="message" placeholder="Votre Message" rows="6"/>
-    //                     </div>
-    //                     <div className={styles.mailinp}>
-    //                         <input name="mail" placeholder="Votre mail" />
-    //                     </div>
-    //                     <div className={styles.sixth}>
-    //                         <label for="adresse"> VOTRE ADRESSE </label>
-    //                         <input name="adresse" placeholder="Votre Adresse" />
-    //                     </div>
-    //                     <div className={styles.seventh}>
-    //                         <img src="https://imgur.com/gallery/EembI94" alt="s" width="41" height="41"/>
-    //                         <button className={styles.addBtn}> + </button>
-    //                     </div>
-    //                     <div className={styles.confirm}>
-    //                         <button type="submit" className={styles.btnEnvoyer}> Envoyer! </button>
-    //                     </div>
-    //                 </div> 
-    //             </form>
-    //         </div>
-    //     </wrapperContact>
-    //   </Container>
-    // </Layout>
