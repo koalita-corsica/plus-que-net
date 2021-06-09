d53e7fdc45cc1458bfba958ff6ca34acb6c0ec9c
tiago-carvalhodev <82232884+tiago-carvalhodev@users.noreply.github.com>
2021-06-09 11:38:43 +0200

    [skip ci]

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,35 +1,24 @@
 // Load variables from `.env` as soon as possible
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV || "development"}`,
-});
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV || 'development'}`
+})
 
-const ImageminPlugin = require("imagemin-webpack-plugin").default;
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require("path");
+const clientConfig = require('./client-config')
 
-const clientConfig = require("./client-config");
-
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   plugins: [
-    "gatsby-plugin-postcss",
-    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: "gatsby-source-sanity",
+      resolve: 'gatsby-source-sanity',
       options: {
         ...clientConfig.sanity,
         token: process.env.SANITY_READ_TOKEN,
         watchMode: !isProd,
-        overlayDrafts: !isProd,
-      },
-    },
-    new CopyWebpackPlugin([
-      {
-        from: "img/**/**",
-        to: path.resolve(__dirname, "dist"),
-      },
-    ]),
-    new ImageminPlugin(),
-  ],
-};
+        overlayDrafts: !isProd
+      }
+    }
+  ]
+}
