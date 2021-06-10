7bbf58550fbe46f1fcf44056f3a618c8a779fb37
tiago-carvalhodev <82232884+tiago-carvalhodev@users.noreply.github.com>
2021-06-10 07:52:42 +0200

    [skip cli]

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -1,153 +1,186 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-return-assign */
-import {Link, StaticQuery, graphql} from 'gatsby'
-import React, {useState} from 'react'
+import { Link, StaticQuery, graphql } from "gatsby";
+import React, { useState } from "react";
 
-import styled from 'styled-components'
-import styles from './header.module.css'
+import styled from "styled-components";
+import styles from "./header.module.css";
 
 const MenuIcon = styled.div`
-  display: ${({show}) => show === false ? 'none' : 'none'};
-@media (max-width: 768px) {
-  position: absolute;
-  top: 2rem;
-  left: ${({nav}) => nav ? '' : '2rem'};
-  right: ${({nav}) => nav ? '2rem' : ''};
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  width: 1.5rem;
-  height: 1.5rem;
-   border: none;
-   cursor: pointer;
-   z-index: 100;
-
-   div {
+  display: ${({ show }) => (show === false ? "none" : "none")};
+  @media (max-width: 768px) {
+    position: absolute;
+    top: 2rem;
+    left: ${({ nav }) => (nav ? "" : "2rem")};
+    right: ${({ nav }) => (nav ? "2rem" : "")};
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
     width: 1.5rem;
-    height: .33rem;
-    background: ${({nav}) => nav ? '#f26633' : 'white'};
-    border-radius: 5px;
-    transform-origin: 1px;
-    transition: transform 300ms;
-    z-index: 900;
+    height: 1.5rem;
+    border: none;
+    cursor: pointer;
+    z-index: 100;
 
-    :nth-child(3) {
-      width: ${({nav}) => nav ? '1.5rem' : '1rem'}
-    }
+    div {
+      width: 1.5rem;
+      height: 0.33rem;
+      background: ${({ nav }) => (nav ? "#f26633" : "white")};
+      border-radius: 5px;
+      transform-origin: 1px;
+      transition: transform 300ms;
+      z-index: 900;
 
-    :first-child{
-      transform: ${({nav}) => nav ? 'rotate(45deg)' : 'rotate(0)'}
-    }
-    :nth-child(2){
-      opacity: ${({nav}) => nav ? '0' : '1'}
-    }
-    :nth-child(3) {
-      transform: ${({nav}) => nav ? 'rotate(-45deg)' : 'rotate(0)'}
+      :nth-child(3) {
+        width: ${({ nav }) => (nav ? "1.5rem" : "1rem")};
+      }
+
+      :first-child {
+        transform: ${({ nav }) => (nav ? "rotate(45deg)" : "rotate(0)")};
+      }
+      :nth-child(2) {
+        opacity: ${({ nav }) => (nav ? "0" : "1")};
+      }
+      :nth-child(3) {
+        transform: ${({ nav }) => (nav ? "rotate(-45deg)" : "rotate(0)")};
+      }
     }
   }
-}
-`
+`;
 const MenuLinks = styled.nav`
   display: flex;
   justify-content: space-evenly;
   list-style-type: none;
   width: 100%;
   img {
-    display:none;
+    display: none;
   }
-@media (max-width: 768px) {
-  text-transform: uppercase;
-  flex-direction: column;
-  text-align: center;
-  height: 100vh;
-  width: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  display: ${({nav}) => (nav ? 'block' : 'none')};
-  background-color: #262626;
-  z-index: 90;
+  @media (max-width: 768px) {
+    text-transform: uppercase;
+    flex-direction: column;
+    text-align: center;
+    height: 100vh;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: ${({ nav }) => (nav ? "block" : "none")};
+    background-color: #262626;
+    z-index: 90;
 
-   ul {
-     list-style-type: none;
-     flex-direction: column;
-  }
-  li {
-    margin-top: 1rem;
-    margin-left: -15%;
-  }
-  a {
-    text-decoration: none;
-    color: #FFFFFF;
-    font-size: 1.5rem;
-    transition: color 300ms;
-    :hover{
-      color: #f26633;
+    ul {
+      list-style-type: none;
+      flex-direction: column;
+    }
+    li {
+      margin-top: 1rem;
+      margin-left: -15%;
+    }
+    a {
+      text-decoration: none;
+      color: #ffffff;
+      font-size: 1.5rem;
+      transition: color 300ms;
+      :hover {
+        color: #f26633;
+      }
+    }
+    img {
+      display: inline-block;
+      margin-left: -5.5vw;
     }
   }
-  img {
-    display: inline-block;
-    margin-left: -5.5vw;
-    
-  }
-}
-`
+`;
 
-const Header = ({nav, showNav, show, data}) => (
+const Header = ({ nav, showNav, show, data }) => (
   <React.Fragment>
     <div className={styles.root}>
-      <MenuIcon nav={nav} onClick={() => showNav(!nav)} styles={{display: 'none'}}>
+      <MenuIcon nav={nav} onClick={() => showNav(!nav)} styles={{ display: "none" }}>
         <div />
         <div />
         <div />
       </MenuIcon>
       <div className={styles.navBar}>
-        <Link to='/'> <img src={data.sanitySiteSettings.mainImage.asset.url} width='256' alt='Plus-que-net' id='logo' /> </Link>
+        <Link to="/">
+          {" "}
+          <img
+            src={data.sanitySiteSettings.mainImage.asset.url}
+            width="256"
+            height="169.14"
+            alt="Plus-que-net"
+            id="logo"
+          />{" "}
+        </Link>
       </div>
       <div className={styles.logo}>
-        <Link to='/'> <img src={data.sanitySiteSettings.mainImage.asset.url} width='256' alt='Plus-que-net' id='logo' /> </Link>
+        <Link to="/">
+          {" "}
+          <img
+            src={data.sanitySiteSettings.mainImage.asset.url}
+            width="256"
+            height="169.14"
+            alt="Plus-que-net"
+            id="logo"
+          />{" "}
+        </Link>
       </div>
       <MenuLinks nav={nav}>
-      <Link to='/'> <img src={data.sanitySiteSettings.mainImage.asset.url} width='256' alt='Plus-que-net' id='logo' /> </Link>
+        <Link to="/">
+          {" "}
+          <img
+            src={data.sanitySiteSettings.mainImage.asset.url}
+            width="256"
+            alt="Plus-que-net"
+            id="logo"
+          />{" "}
+        </Link>
         <div className={styles.nav}>
           <ul>
-            {data.sanitySiteSettings.menu.map(item =>
+            {data.sanitySiteSettings.menu.map((item) => (
               <React.Fragment>
                 <li>
-                  <Link activeStyle={{color: '#F26633'}} to={item.page.slug.current === 'accueil' ? '/' : '/' + `${item.page.slug.current}`} > {item.page.title}</Link>
+                  <Link
+                    activeStyle={{ color: "#F26633" }}
+                    to={
+                      item.page.slug.current === "accueil" ? "/" : "/" + `${item.page.slug.current}`
+                    }
+                  >
+                    {" "}
+                    {item.page.title}
+                  </Link>
                 </li>
               </React.Fragment>
-            )}
+            ))}
           </ul>
         </div>
       </MenuLinks>
     </div>
   </React.Fragment>
-)
-export default function MyHeader (props) {
+);
+export default function MyHeader(props) {
   return (
     <StaticQuery
       query={graphql`
-      query HeadingQuery {
-        sanitySiteSettings {
-          title
-          mainImage {
-            asset {
-              url
+        query HeadingQuery {
+          sanitySiteSettings {
+            title
+            mainImage {
+              asset {
+                url
+              }
             }
-          }
-          menu {
-            page {
-              title
-              slug {
-                current
+            menu {
+              page {
+                title
+                slug {
+                  current
+                }
               }
             }
           }
         }
-      }
       `}
-      render={data => <Header data={data} {...props} />}
+      render={(data) => <Header data={data} {...props} />}
     />
-  )
+  );
 }
